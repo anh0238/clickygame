8e18153af69a9303d58e86802c82e4b8f0550332
ahend <anh0238@gmail.com>
2018-12-19 20:52:13 -0600

    Updates

---
+++ precache-manifest.035eb31cc07766676827b0b78478abd4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4be6aafe5a175e721bceb90888169d1",
+    "url": "/clickygame/static/media/treefour.d4be6aaf.png"
+  },
+  {
+    "revision": "68a858df1aa1c54c3673",
+    "url": "/clickygame/static/css/main.d46f18b1.chunk.css"
+  },
+  {
+    "revision": "1d73500619109f487b00",
+    "url": "/clickygame/static/js/1.1d735006.chunk.js"
+  },
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "b50bf93b6a54b7a6b02561f9980c3c8e",
+    "url": "/clickygame/static/media/treeone.b50bf93b.png"
+  },
+  {
+    "revision": "5783d844bde981f328855b732aa1ce2e",
+    "url": "/clickygame/static/media/treetwo.5783d844.png"
+  },
+  {
+    "revision": "0d433a3803d2171bdb6cc0bad271ecca",
+    "url": "/clickygame/static/media/treethree.0d433a38.png"
+  },
+  {
+    "revision": "68a858df1aa1c54c3673",
+    "url": "/clickygame/static/js/main.68a858df.chunk.js"
+  },
+  {
+    "revision": "f87578b41978ebb5e63ce74f5c9f4dd5",
+    "url": "/clickygame/static/media/treefive.f87578b4.png"
+  },
+  {
+    "revision": "b28de937f64c92a4e97e25e3b28253f2",
+    "url": "/clickygame/static/media/treesix.b28de937.png"
+  },
+  {
+    "revision": "78fad0c0d8248bac50b8bafc13d1f580",
+    "url": "/clickygame/static/media/treeseven.78fad0c0.png"
+  },
+  {
+    "revision": "dc7f6c847712df758bdb829b11c9dab0",
+    "url": "/clickygame/static/media/treeeight.dc7f6c84.png"
+  },
+  {
+    "revision": "597000b6c887d7fd33d923d8f0bc69ec",
+    "url": "/clickygame/static/media/treenine.597000b6.png"
+  },
+  {
+    "revision": "7da77186b4f2ef3991bf102de0b7bc9a",
+    "url": "/clickygame/index.html"
+  }
+];
